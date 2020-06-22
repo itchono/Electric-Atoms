48a04a840727f20de79ab3809fe83b739d9c83bd
Mingde Yin <54449457+itchono@users.noreply.github.com>
2020-06-22 00:06:59 -0400

    working pog?

--- case 2/case2v2.py
+++ case 2/case2v2.py
@@ -9,10 +9,10 @@ def hamiltonian(ang_freq, B_0, end_time, num_steps):
     H_0[:, 1, 1].fill(2*np.pi*177*10**6)
 
     H_int = np.zeros((num_steps, 2, 2))
-    H_int[:, 0, 1] = B_0*2*np.pi*10**6*np.cos(ang_freq*np.linspace(0, end_time, num_steps))
-    H_int[:, 1, 0] = B_0*2*np.pi*10**6*np.cos(ang_freq*np.linspace(0, end_time, num_steps))
+    H_int[:, 0, 1] = B_0*2*np.pi*10**6*np.cos(np.linspace(0, end_time, num_steps))
+    H_int[:, 1, 0] = B_0*2*np.pi*10**6*np.cos(np.linspace(0, end_time, num_steps))
 
-    return (H_0 + H_int).astype(complex) / (2*np.pi*177*10**6) # normalize to energy
+    return (H_0 + H_int).astype(complex)
 
 def propagate(starting_state, ang_freq, B, end_time, num_steps):
     '''
@@ -24,15 +24,10 @@ def propagate(starting_state, ang_freq, B, end_time, num_steps):
 
     H = hamiltonian(ang_freq, B, end_time, num_steps)
 
-    print(H[0, :, :])
-
     dt = complex(end_time / num_steps) # timestep
 
     for i in range(num_steps-1):
-
-        dr = (np.matmul(H[i,:,:], result[i,:,:]) - np.matmul(result[i,:,:], H[i,:,:]))/(0+1j) * dt
-
-        result[i+1,:,:] = result[i,:,:] + dr
+        result[i+1, :,:] = result[i,:,:] + (np.matmul(H[i,:,:], result[i,:,:]) - np.matmul(result[i,:,:], H[i,:,:]))/(0+1j) * dt
 
     return result
 
@@ -40,9 +35,14 @@ def propagate(starting_state, ang_freq, B, end_time, num_steps):
 if __name__ == "__main__":
     P_0 = np.array([[1,0], [0, 0]]) # Initial Starting State
 
-    P_t = propagate(P_0, 0, 0.01, 10**-4, 100)
+    P_t = propagate(P_0, 0, 0.01, 10**-4, 10)
     # DC mode; no transitions are driven, as expected
 
     print(P_t[-1,:, :])
+
+    ''' P_t = propagate(P_0, 2, 0.01, 10, 10)
+        # 200 kpi radians per second
+
+        print(P_t[-1,:,:])'''
 
     
