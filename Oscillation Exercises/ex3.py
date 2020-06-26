68ce95f258f3bb3ffe98199b582ed3767f7b6458
Mingde Yin <mingdeyin@gmail.com>
2020-06-26 00:10:18 -0400

    plotdone

--- Oscillation Exercises/ex3.py
+++ Oscillation Exercises/ex3.py
@@ -3,7 +3,13 @@ from scipy import linalg as lg
 from numpy import pi,sin,cos,tan,sqrt
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
+import matplotlib.ticker as ticker
 
+
+# units in MHz
+TRANSITON_FREQUENCY = 177
+OSCILLATION_FREQUENCY = 177
+RABI_FREQUENCY = 10
 
 def solve(TRANSITON_FREQUENCY, OSCILLATION_FREQUENCY, RABI_FREQUENCY):
     ## density matrix (much faster)
@@ -20,30 +26,44 @@ def solve(TRANSITON_FREQUENCY, OSCILLATION_FREQUENCY, RABI_FREQUENCY):
 
 
     # solution
-    t = np.linspace(0,1,500) # time units in terms of microseconds
+    t = np.linspace(0,0.05,500) # time units in terms of microseconds
 
     r_init = np.array([0,0,0]) # initial starting state of the DEs
+
 
     w,w0 = 2*pi*TRANSITON_FREQUENCY,2*pi*OSCILLATION_FREQUENCY # forced oscillation frequency vs energy level frequency
     Omega = 2*pi*RABI_FREQUENCY
 
+
     solution = odeint(equation_system, r_init, t, args=(Omega,w,w0))
 
-    rho_11 = 1-solution[:,0][-1]
+    b_z = 2*solution[:,0] - 1
+    b_x = 2*solution[:,1]
+    b_y = -2*solution[:,2]
 
-    print("Done")
 
-    return rho_11
+    return b_x, b_y, b_z
 
-w = np.linspace(100, 250, 400)
 
-prob = []
+fig = plt.figure()
 
-for i in w:
-    prob.append(solve(177, i, 10))
+bx, by, bz = solve(TRANSITON_FREQUENCY, OSCILLATION_FREQUENCY, RABI_FREQUENCY)
 
-print(prob)
+ax = fig.add_subplot(111, projection='3d')
 
-plt.plot(w, np.array(prob))
+ax.set_xlabel("$b_x$")
+ax.set_ylabel("$b_y$")
+ax.set_zlabel("$b_z$")
+
+ax.plot3D([0],[0],[1],'ro')
+ax.plot3D([0],[0],[-1],'go')
+
+ax.text(0,0,1.2,r"$|1\rangle$")
+ax.text(0,0,-1.2,r"$|0\rangle$")
+
+tick_spacing = 0.5
+
+for axis in [ax.xaxis,ax.yaxis,ax.zaxis]:
+    axis.set_major_locator(ticker.MultipleLocator(tick_spacing))
+ax.plot3D(bx,by,bz,lw=2)
 plt.show()
-
