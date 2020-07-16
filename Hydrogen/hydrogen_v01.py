f8bcbae68c59a65674e9ed961dff41af05e792a5
Mingde Yin <mingdeyin@gmail.com>
2020-07-16 04:31:27 -0400

    Update hydrogen_v01.py

--- Hydrogen/hydrogen_v01.py
+++ Hydrogen/hydrogen_v01.py
@@ -3,7 +3,9 @@
 
 import numpy as np
 
-
+'''
+Trajectory
+'''
 targetvolume = np.ones((30, 15, 15)) # x is beam direction, z is B field, y is horizonal transverse
 
 START_POINT = (0, 7, 7) # start at centre of one end
@@ -14,6 +16,20 @@ VEL_VECTOR = VEL_VECTOR / np.linalg.norm(VEL_VECTOR)
 
 SPEED = 10
 
+STEP_NUMBER = 100
 
+PASS_POINTS = np.linspace(START_POINT, START_POINT+VEL_VECTOR*30, STEP_NUMBER)
 
-PASS_POINTS = 
+B = 0
+
+for coord in PASS_POINTS:
+    B.append(targetvolume[coord]) # this is basically the idea
+
+'''
+Calculation
+'''
+
+def H():
+    # -mu dot B
+    # using the B's from above
+    times = 0 # somehow relate the times with the positions
