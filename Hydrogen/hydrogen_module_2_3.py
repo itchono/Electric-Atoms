465abd0be7b65f7b7ea8aa429ae94213beaa26c2
Mingde Yin <mingdeyin@gmail.com>
2020-07-17 03:32:57 -0400

    Update hydrogen_module_2_3.py

--- Hydrogen/hydrogen_module_2_3.py
+++ Hydrogen/hydrogen_module_2_3.py
@@ -18,6 +18,8 @@ def filter(particles, aperture_diameter = 0.5, axial_length = 30):
     particles: array containing some 5x1 vectors representing particles (vz, vy, z, y, vx)
     aperture_diameter: diameter of terminal aperture, in cm
     axial_length: distance that particles must travel in x direction to reach aperture
+
+    Should be working well
     '''
     radius = aperture_diameter / 2 # convert diameter to radius
 
@@ -31,8 +33,6 @@ def filter(particles, aperture_diameter = 0.5, axial_length = 30):
     Ymask = np.nonzero(np.abs(Y) < radius) # indices where Y is okay
 
     return particles[np.intersect1d(Zmask, Ymask)] # takes indices where both Y and Z are okay
-
-print(filter(np.array([STATE]*5)))
     
 def positions(particles, axial_length = 30, num_steps = 500):
     '''
@@ -41,8 +41,9 @@ def positions(particles, axial_length = 30, num_steps = 500):
     particles: array containing some 5x1 vectors representing particles (vz, vy, z, y, vx)
     axial_length: distance that particles must travel in x direction to reach aperture
     num_steps: number of desired timesteps for position
-    '''
 
+    Currently: Does not allow variable time steps
+    '''
     vz, vy, z, y, vx = particles.T # unpack each particle
 
     T = axial_length / vx # get Times taken to traverse the x distance
@@ -54,8 +55,6 @@ def positions(particles, axial_length = 30, num_steps = 500):
     Z = z + vz * times
     # apply newton's law
 
-    print("hello", X)
+    return np.transpose(np.array((X, Y, Z)), [2, 0, 1])
 
-    #return np.array([X, Y, Z])
-
-print(positions(np.array([STATE]*2), num_steps=10))
+print(positions(np.array([STATE, STATE2]), num_steps=4))
