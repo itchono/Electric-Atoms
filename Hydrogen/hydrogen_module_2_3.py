af65587289d61c3d292b1ba3ea4ff3b9b46bb04b
Mingde Yin <mingdeyin@gmail.com>
2020-07-17 03:02:28 -0400

    Update hydrogen_module_2_3.py

--- Hydrogen/hydrogen_module_2_3.py
+++ Hydrogen/hydrogen_module_2_3.py
@@ -1,43 +1,54 @@
 # Hydrogen
-# Checker and velocity extractor functions
+# Checker (2) and velocity extractor functions (3)
 # all lengths in cm
 # all times in s
 
 import numpy as np
 
-vz, vy, z, y, vx = 0,0,0,0,200000
 
 # np.loadtxt(fname)
 
-STATE = np.array((vz, vy, z, y, vx))
+STATE = np.array((0,0,0,0,200000)) # Tester State; remove in final
 
-
-def filter(aperture_diameter = 0.5, axial_length = 30, particles = np.array([STATE]*5)):
+def filter(particles, aperture_diameter = 0.5, axial_length = 30):
     '''
-    aperture_diameter: diameter of terminal aperture, in mm
+    Filters out particles that would not reach the aperture.
+
+    particles: array containing some 5x1 vectors representing particles (vz, vy, z, y, vx)
+    aperture_diameter: diameter of terminal aperture, in cm
     axial_length: distance that particles must travel in x direction to reach aperture
-    particles: array of 5x1 vectors representing particles
     '''
-
     radius = aperture_diameter / 2
 
-    '''
-    vz, vy, z, y, vx = STATE
-
-    T = axial_length / vx
-    Z = z + vz * T
-    Y = y + vy * T
-
-    check = -radius < Z < radius and -radius < Y < radius
-    '''
+    vz, vy, z, y, vx = particles.T
 
     T = axial_length / particles[:,4] # get Times taken to traverse the x distance
     Z = particles[:,2] + particles[:,0] * T # get final Z at time T
     Y = particles[:,3] + particles[:,1] * T # get final Y at time T
 
-    return particles[np.nonzero(-radius < Z < radius and -radius < Y < radius)]
+    Zmask = np.nonzero(np.abs(Z) < radius) # indices where Z is okay
+    Ymask = np.nonzero(np.abs(Y) < radius) # indices where Y is okay
+    mask = np.intersect1d(Zmask, Ymask)
 
-print(filter())
+    return particles[mask]
+
+print(filter([STATE]*5))
     
+
+
+def positions(particles, axial_length = 30, num_steps = 500):
+    '''
+    Gets the position over time functions of particles
+
+    particles: array containing some 5x1 vectors representing particles (vz, vy, z, y, vx)
+    axial_length: distance that particles must travel in x direction to reach aperture
+    num_steps: number of desired timesteps for position
+    '''
+
+    T = axial_length / particles[:,4] # get Times taken to traverse the x distance
+
+    times = np.linspace(0, T, num=num_steps)
+
+    X = particles[:,4] * times
 
     
