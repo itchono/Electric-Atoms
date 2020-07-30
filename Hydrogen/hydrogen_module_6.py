faccfe480e494611ebf105ad885a1818c2fa4544
Mingde Yin <mingdeyin@gmail.com>
2020-07-30 02:55:29 -0400

    Update hydrogen_module_6.py

--- Hydrogen/hydrogen_module_6.py
+++ Hydrogen/hydrogen_module_6.py
@@ -36,3 +36,19 @@ def unitary(H_array, dt):
     return U
 
 ## Construct differential equations?
+
+def equation_system(r,t,Omega,w0,w):
+    # Need 9 DEs
+
+    # Problem: can't solve magnetic fields analytically <- uh oh
+
+    rho_0011, rho_1122
+
+    rho_00, rho_10_r, rho_10_i = r
+    rhodot_00 = 2*rho_10_i * Omega*cos(w*t)
+    rhodot_10_r = w0*rho_10_i
+    rhodot_10_i = -w0*rho_10_r - (2*rho_00 - 1) * Omega*cos(w*t)
+    return rhodot_00, rhodot_10_r, rhodot_10_i
+
+
+solution = odeint(equation_system,r_init,t,args=(Omega,w,w0))
